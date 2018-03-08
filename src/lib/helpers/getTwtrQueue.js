3e02b2778eca38767b2b0e6471c11b8872692f8d
Serban Stancu <stancu@adobe.com>
2018-03-08 16:30:29 -0700

    Update Twitter initialization snippet.

--- src/lib/helpers/getTwtrQueue.js
+++ src/lib/helpers/getTwtrQueue.js
@@ -3,36 +3,28 @@
 var window = require('@adobe/reactor-window');
 var loadScript = require('@adobe/reactor-load-script');
 var extensionSettings = turbine.getExtensionSettings();
-var twtr;
 
-var createTwtrQueue = function() {
-  var twtr = function() {
-    twtr.callMethod ? twtr.callMethod.apply(twtr, arguments) : twtr.queue.push(arguments);
+var createTwitterQueue = function() {
+  var twq = function() {
+    twq.exe ? twq.exe.apply(twq, arguments) : twq.queue.push(arguments);
   };
 
-  twtr.push = twtr;
-  twtr.loaded = true;
-  twtr.version = '1.0';
-  twtr.queue = [];
+  twq.version = '1.1';
+  twq.queue = [];
 
-  return twtr;
+  return twq;
 };
 
-if (!window.twtr) {
-  twtr = createTwtrQueue();
+if (!window.twq) {
+  window.twq = createTwitterQueue();
 
-  window.twtr = twtr;
-  if (!window._twtr) {
-    window._twtr =   twtr;
-  }
+  loadScript('https://static.ads-twitter.com/uwt.js').then(function() {
+    turbine.logger.log('Twitter Pixel Base Code was successfully loaded.');
+  }, function() {
+    turbine.logger.error('Twitter Pixel Base Code could not be loaded.');
+  });
 }
 
-loadScript('https://static.ads-twitter.com/uwt.js').then(function() {
-  turbine.logger.log('Twitter Pixel Base Code was successfully loaded.');
-}, function() {
-  turbine.logger.error('Twitter Pixel Base Code could not be loaded.');
-});
+window.twq('init', extensionSettings.account_id);
 
-window.twtr('init', extensionSettings.account_id);
-
-module.exports = twtr;
+module.exports = window.twq;
