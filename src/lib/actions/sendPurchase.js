781952bea7d3f85ba0895b0edd1dc6b9c904d665
Serban Stancu <stancu@adobe.com>
2018-03-21 14:18:47 -0600

    Send purchase type.

--- src/lib/actions/sendPurchase.js
+++ src/lib/actions/sendPurchase.js
@@ -3,6 +3,6 @@
 module.exports = function(settings) {
   var twtr = require('../helpers/getTwtrQueue');
 
-  twtr('track', 'Conversion', settings);
-  turbine.logger.log('Queue command: twtr("track", "Conversion", ' + JSON.stringify(settings) + ').');
+  twtr('track', 'Purchase', settings);
+  turbine.logger.log('Queue command: twtr("track", "Purchase", ' + JSON.stringify(settings) + ').');
 };
